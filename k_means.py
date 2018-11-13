135258cf3cf12d96cb96dfd12c138aaced6c45b7
Remaldeep <technoremal@gmail.com>
2018-11-12 22:27:18 -0700

    [k-means] fixed dictionaries

--- k_means.py
+++ k_means.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import StandardScaler
+from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from collections import defaultdict
@@ -42,25 +43,34 @@ def local_search(U, C, k, u_dict):
         for i in range(U.shape[0]):
             C_min = C.copy()
             min_cost = cost
+            min_u_dict = u_dict.copy()
+            if i in C:
+                continue
             for c in C.copy(): #Check is c is not changed.
                 C.remove(c)
                 C.add(i)
+                u_dict.clear()
+                for c_i in C:
+                    u_dict[c_i] = c_i
                 cost_new = cost_km(C, U, u_dict)
                 if (cost_new < min_cost):
                     C_min = C.copy()
                     min_cost = cost_new
+                    min_u_dict = u_dict.copy()
                 C.remove(i)
                 C.add(c)
             C = C_min.copy()
             cost = min_cost
+            u_dict = min_u_dict.copy()
             if(i%1000 == 0):
                 print(str(i)+" done")
         #There is no need for c prime.
-    return C
+    return C, u_dict
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('../shuttle.tst', header=None, sep=' ')
+    n_rows = 100
+    df = pd.read_csv('../shuttle.tst', header=None, sep=' ', nrows=n_rows)
     print(df.shape[0])
     print(df.head())
     print(df.iloc[1:2])
@@ -79,14 +89,22 @@ if __name__ == '__main__':
     #C_val = random.sample(range(x.shape[0]), 7)
     #C.update(C_val)
     #print(C)
-    C = {10371, 13927, 169, 12683, 77, 3578, 7386} #Chosen at random
-    k=7
+    #C = {10371, 13927, 169, 12683, 77, 3578, 7386}  # Chosen at random
+    C = {34, 38, 43, 13, 81, 56, 58}
+    k = 7
+
+    #x = np.array([[1,1],[1,2],[2,1],[2,2],[100,100],[100,101],[101,100],[101,101]])
+    #y = np.array([1,1,1,1,2,2,2,2])
+    #C = {0, 1}
+    #k = 2
+
+
 
     u_dict = defaultdict(dict)
     for val in C:
         u_dict[val] = val
 
-    local_search(x, C, k, u_dict)
+    C, u_dict = local_search(x, C, k, u_dict)
 
     #Visualizing the clusters.
     color_dict = defaultdict(str) #Holds the color for each center now.
@@ -100,9 +118,12 @@ if __name__ == '__main__':
         colors.append(color_dict[center])
 
     pca = PCA(n_components=2)
-    x_pca = pca.fit_transform(x)
-    plt.scatter(x_pca[:, 0], x_pca[:, 1], c=colors, alpha=0.5)
-    plt.show()
+    tsne = TSNE(n_components=2)
+    x_modified = pca.fit_transform(x)
+    #x_modified = tsne.fit_transform(x)
+    plt.figure(1)
+    plt.scatter(x_modified[:, 0], x_modified[:, 1], c=colors, alpha=0.8)
+    plt.savefig('plt.png')
 
 
 
